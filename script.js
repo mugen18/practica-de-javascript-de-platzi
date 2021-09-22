4615759a292134a5e4870dba9a34f1000f617e3f
jhonnyescobar <jhalexander18@gmail.com>
2021-09-22 10:40:50 -0500

    se realiza maqutacuon  de botones de calculos perimetrales con javaScript y se dan estilos css.

--- script.js
+++ script.js
@@ -1,4 +1,52 @@
-const ladoCuadrado = 5;
-console.log("los lados del cuadrado miden:"+ ladoCuadrado);
+console.log("Hello wrold");
+console.group("Cuadrados");  
 
-const PerimetroCuadrado = ladoCuadrado*4
+//codigo del cuadrado
+//const ladoCuadrado = 5;
+//function ladosDelCuadrado(lado) {
+  //  return lado; 
+
+ 
+//console.log("los lados del cuadrado miden: "+ lado +"cm");
+
+function PerimetroCuadrado(lado) {
+    return lado*4;
+
+}
+//console.log("el perimetro del cuadrado es: "+ PerimetroCuadrado  +"cm");
+
+function AreaCuadrado(lado) {
+    return lado * lado;
+}
+//console.log("el Area del cuadrado es: "+ AreaCuadrado  +"cm");
+
+console.groupEnd();
+
+console.group("Triangulos");  
+    function PerimetroTriangulo(lado) {
+    return lado*3;
+}
+    console.groupEnd();
+
+
+
+//interaccion con javaScript
+function calcularPerimetrocuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    const perimetro ="el perimetro es "+ PerimetroCuadrado(value);
+    alert(perimetro);
+
+}
+
+
+//interaccion con javaScript, triangulo
+
+
+
+function calcularPerimetroTriangulo() {
+    const input = document.getElementById("InputTriangulo");
+    const value = input.value
+    const perimetroTriangulo = "el perimetro del trisangulo es: " + PerimetroTriangulo(value);
+    alert(perimetroTriangulo);
+}
